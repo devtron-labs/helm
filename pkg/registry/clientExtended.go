15cb5cafbbea3cb1b2228aedd289ca551434eb02
ayushmaheshwari <ayush@devtron.ai>
2024-01-23 16:16:32 +0530

    removing conversion function

--- pkg/registry/clientExtended.go
+++ pkg/registry/clientExtended.go
@@ -2,7 +2,6 @@ package registry // import "helm.sh/helm/v3/pkg/registry"
 import (
 	"oras.land/oras-go/pkg/registry"
 	registryremote "oras.land/oras-go/pkg/registry/remote"
-	"strings"
 )
 
 // GetTagsIgnoreSemVer implements Tags function but removes semver StrictNewVersion check
@@ -19,20 +18,10 @@ func (c *Client) GetTagsIgnoreSemVer(ref string) ([]string, error) {
 	}
 
 	var registryTags []string
-
 	registryTags, err = registry.Tags(ctx(c.out, c.debug), &repository)
 	if err != nil {
 		return nil, err
 	}
 
-	var tagVersions []string
-	for _, tag := range registryTags {
-		// Change underscore (_) back to plus (+) for Helm
-		// See https://github.com/helm/helm/issues/10166
-		tagVersion := strings.ReplaceAll(tag, "_", "+")
-		tagVersions = append(tagVersions, tagVersion)
-
-	}
-
-	return tagVersions, nil
+	return registryTags, nil
 }
